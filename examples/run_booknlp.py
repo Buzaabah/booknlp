8a08bcf8215cc30e9490c1e8bb22af8cece5c4d8
Happy Buzaaba <hbuzaaba@gmail.com>
2024-05-06 17:48:33 -0400

    added book

--- examples/run_booknlp.py
+++ examples/run_booknlp.py
@@ -8,12 +8,14 @@ model_params={
 booknlp=BookNLP("en", model_params)
 
 # Input file to process
-input_file="158_emma.txt"
+#input_file="158_emma.txt"
+input_file="Ireke_Onibudo.txt"
 
 # Output directory to store resulting files in
-output_directory="158_emma"
+#output_directory="158_emma"
+output_directory="Ireke_Onibudo"
 
 # File within this directory will be named ${book_id}.entities, ${book_id}.tokens, etc.
-book_id="158_emma"
-
+#book_id="158_emma"
+book_id="Ireke_Onibudo"
 booknlp.process(input_file, output_directory, book_id)
