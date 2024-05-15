fe6d21f7649fbe986eda76241ba0881c16e3aee1
Happy Buzaaba <hbuzaaba@gmail.com>
2024-05-15 15:14:16 -0400

    added yoruba text

--- examples/run_booknlp.py
+++ examples/run_booknlp.py
@@ -9,13 +9,13 @@ booknlp=BookNLP("en", model_params)
 
 # Input file to process
 #input_file="158_emma.txt"
-input_file="Ireke_Onibudo.txt"
+input_file="yorREKE_ONIBUDO.txt"
 
 # Output directory to store resulting files in
 #output_directory="158_emma"
-output_directory="Ireke_Onibudo"
+output_directory="yorREKE_ONIBUDO"
 
 # File within this directory will be named ${book_id}.entities, ${book_id}.tokens, etc.
 #book_id="158_emma"
-book_id="Ireke_Onibudo"
+book_id="yorREKE_ONIBUDO"
 booknlp.process(input_file, output_directory, book_id)
