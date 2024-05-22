b4828619efcffe1f235c0eca722eda0608a10c18
Happy Buzaaba <hbuzaaba@gmail.com>
2024-05-22 09:48:45 -0400

    added new books

--- examples/run_booknlp.py
+++ examples/run_booknlp.py
@@ -9,13 +9,13 @@ booknlp=BookNLP("en", model_params)
 
 # Input file to process
 #input_file="158_emma.txt"
-input_file="yorREKE_ONIBUDO.txt"
+input_file="Expedition_to_the_Mount_of_Thought.txt"
 
 # Output directory to store resulting files in
 #output_directory="158_emma"
-output_directory="yorREKE_ONIBUDO"
+output_directory="Expedition_To_Mount"
 
 # File within this directory will be named ${book_id}.entities, ${book_id}.tokens, etc.
 #book_id="158_emma"
-book_id="yorREKE_ONIBUDO"
+book_id="Expedition_to_Mount_of_Thought"
 booknlp.process(input_file, output_directory, book_id)
